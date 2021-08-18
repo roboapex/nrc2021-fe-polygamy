849601ee64bbc245257f2957a4714e7d68bc39d5
jianwen <chin_jian_wen@s2019.ssts.edu.sg>
2021-08-18 16:09:56 +0800

    Changed dependencies

--- main.py
+++ main.py
@@ -1,19 +1,9 @@
 #!/usr/bin/env python3
 import time
 
-'''
-from ev3dev2.motor import LargeMotor, OUTPUT_A, OUTPUT_B, SpeedPercent, MoveTank
-from ev3dev2.sensor.lego import TouchSensor
-from ev3dev2.led import Leds
 from ev3dev2.display import Display
-
-# Import smbus drivers for i2c camera
-
-from ev3dev2.port import LegoPort
-from ev3dev2.sensor import INPUT_1
-'''
-
-from ev3dev2.sensor import Sensor, INPUT_1
+from ev3dev2.motor import LargeMotor, OUTPUT_A, OUTPUT_B, SpeedPercent, MoveTank
+from ev3dev2.sensor import Sensor, INPUT_1, UltrasonicSensor
 from ev3dev2.port import LegoPort
 from smbus import SMBus
 
@@ -30,6 +20,8 @@ address = 0x54
 # Request and read protocal for camera
 data = [174, 193, 32, 2, 1, 1]
 counter = 0
+
+UltrasonicSensor.distance_centimeters()
 
 while True:
     counter += 1
