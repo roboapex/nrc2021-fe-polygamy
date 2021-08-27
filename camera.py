0f63cce4e983b6ad22122dc629fdc52621141857
jianwen <chin_jian_wen@s2019.ssts.edu.sg>
2021-08-28 00:51:36 +0800

    Swapped around the camera code from main to function file
    Changed gitignore to ignore scp files

--- camera.py
+++ camera.py
@@ -1,5 +1,19 @@
 from smbus import SMBus
 
+# Set LEGO port for Pixy2 on input port 1
+in1 = LegoPort(INPUT_1)
+in1.mode = 'other-i2c'
+# Short wait for the port to get ready
+time.sleep(0.5)
+
+ # Settings for I2C (SMBus(3) for INPUT_1)
+bus = SMBus(3)
+address = 0x54
+
+# Request and read protocal for camera
+data = [174, 193, 32, 2, 1, 1]
+counter = 0
+
 def colour():
     counter += 1
     bus.write_i2c_block_data(address, 0, data)
--- main.py
+++ main.py
@@ -5,17 +5,3 @@ from ev3dev2.display import Display
 from ev3dev2.motor import LargeMotor, OUTPUT_A, OUTPUT_B, SpeedPercent, MoveTank
 from ev3dev2.sensor import Sensor, INPUT_1, UltrasonicSensor
 from ev3dev2.port import LegoPort
-
-# Set LEGO port for Pixy2 on input port 1
-in1 = LegoPort(INPUT_1)
-in1.mode = 'other-i2c'
-# Short wait for the port to get ready
-time.sleep(0.5)
-
- # Settings for I2C (SMBus(3) for INPUT_1)
-bus = SMBus(3)
-address = 0x54
-
-# Request and read protocal for camera
-data = [174, 193, 32, 2, 1, 1]
-counter = 0
