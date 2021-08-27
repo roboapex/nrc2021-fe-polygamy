3783578494baf4567b96b6087f5b04b60fc5c9e4
Irman Zaf <irman_zafyree_bin_irmanizam@s2019.ssts.edu.sg>
2021-08-27 14:27:06 +0800

    owo

--- functions.py
+++ functions.py
@@ -4,8 +4,9 @@ print('import')
 import time
 from ev3dev2.motor import LargeMotor, OUTPUT_A, OUTPUT_D
 from ev3dev2.sensor.lego import UltrasonicSensor
-from ev3dev2.sensor import INPUT_3, INPUT_4
+from ev3dev2.sensor import INPUT_3, INPUT_4, INPUT_1
 
+frontUltra = UltrasonicSensor(INPUT_1)
 leftUltra = UltrasonicSensor(INPUT_3)
 rightUltra = UltrasonicSensor(INPUT_4)
 
@@ -16,8 +17,8 @@ rightMotor = LargeMotor(OUTPUT_D)
 speed = 50
 integral = 0
 last_error = 0
-kp = 0.2
-ki = 0.0
+kp = 0.5
+ki = 0.01
 kd = 0
 while True:
     if leftUltra.distance_centimeters > 50:
@@ -28,6 +29,21 @@ while True:
         rightDist = 50
     else:
         rightDist = rightUltra.distance_centimeters
+    if frontUltra.distance_centimeters > 50:
+        frontDist = 50
+    else:
+        frontDist = frontUltra.distance_centimeters
+    if frontDist < 10 :
+        if leftDist < rightDist:
+            while frontDist < 40:
+                leftMotor.on(50,brake=True,block=False)
+                rightMotor.on(20,brake=True,block=False)
+        elif leftDist > rightDist:
+            while frontDist < 40:
+                leftMotor.on(20,brake=True,block=False)
+                rightMotor.on(50,brake=True,block=False)
+        else:
+            pass
     error = leftDist - rightDist
     integral = error + integral
     derivative = error - last_error
