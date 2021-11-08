53d3996fc584cf8173ee2b3465cd3780031996e5
jianwen-android <65391811+jianwen-android@users.noreply.github.com>
2021-11-08 16:05:05 +0800

    sadfa

--- code/camera.py
+++ code/camera.py
@@ -1,44 +1,43 @@
 #!/usr/bin/env python3
 from time import sleep
-from ev3dev2.sensor import Sensor, INPUT_1
-
+from ev3dev2.sensor import Sensor, INPUT_4
 from ev3dev2.port import LegoPort
-from ev3dev2.motor import LargeMotor, OUTPUT_A, OUTPUT_D, OUTPUT_B, MediumMotor
 
 from smbus import SMBus
 
-print('done importing')
-
 # Set LEGO port for Pixy2 on input port 4
-in1 = LegoPort(INPUT_1)
+in1 = LegoPort(INPUT_4)
 in1.mode = "other-i2c"
 # Short wait for port to get ready
 sleep(0.5)
 
 # Settings for I2C (SMBus(6)) for INPUT_4
-bus = SMBus(3)
+bus = SMBus(6)
 address = 0x54
 
-frontUltra = UltrasonicSensor(INPUT_1)
-leftUltra = UltrasonicSensor(INPUT_2)
-rightUltra = UltrasonicSensor(INPUT_3)
+data = [174, 193, 14, 0]
+bus.write_i2c_block_data(address, 0, data)
+block = bus.read_i2c_block_data(address, 0, 13)
+print("Firmware version: {}.{}\n".format(str(block[8]), str(block[9])))
 
-counter = 0
+traffic = []
 
-frontUltra.Distance - Centimeters
+# Sigmap 1 is trained on red
+# Sigmap 2 is trained on green
 
 
-def colour():
-    counter += 1
+def colour(sigmap):
+    sigmap = int(sigmap)
+    data = [174, 193, 32, 2, sigmap, 1]
     bus.write_i2c_block_data(address, 0, data)
     block = bus.read_i2c_block_data(address, 0, 20)
-    print(block, block[19])
+
     time.sleep(0.1)
-    """
-    sig = block[7]*256 + block[6]
-    x = block[9]*256 + block[8]
-    y = block[11]*256 + block[10]
-    w = block[13]*256 + block[12]
-    h = block[15]*256 + block[14]
-    """
-    # print("sig: {} | x: {} | y: {} | w: {} | h: {}".format(sig, x, y, w, h))
+
+    if (block[7] * 256 + block[6]) == sigmap:
+        x = block[9] * 256 + block[8]
+        w = block[13] * 256 + block[12]
+        h = block[15] * 256 + block[14]
+        traffic.append([w * h, x])
+
+    return traffic
--- code/main.py
+++ code/main.py
@@ -1,7 +1,122 @@
 #!/usr/bin/env python3
 import time
+import asyncio
 
 from ev3dev2.display import Display
-from ev3dev2.motor import LargeMotor, OUTPUT_A, OUTPUT_B, SpeedPercent, MoveTank
-from ev3dev2.sensor import Sensor, INPUT_1, UltrasonicSensor
+from ev3dev2.motor import MediumMotor, OUTPUT_A, OUTPUT_B, SpeedPercent, MoveTank
 from ev3dev2.port import LegoPort
+from ev3dev2.sensor import INPUT_1, INPUT_2, INPUT_3, INPUT_4, UltrasonicSensor
+
+from camera.py import colour()
+
+frontUltra = UltrasonicSensor(INPUT_1)
+leftUltra = UltrasonicSensor(INPUT_2)
+rightUltra = UltrasonicSensor(INPUT_3)
+
+leftMotor = MediumMotor(OUTPUT_B)
+rightMotor = MediumMotor(OUTPUT_D)
+
+sped = 30"
+
+# Sigmap 1 is trained on red
+# Sigmap 2 is trained on green
+
+def forwardTill(speed,dist):
+    while frontUltra.distance_centimeters > dist:
+        leftMotor.on((speed)*-1,brake=True,block=False)
+        rightMotor.on((speed),brake=True,block=False)
+
+    leftMotor.stop()
+    rightMotor.stop()
+
+def spinTurn(speed,dist):
+    ratio = 0.87 / 2
+    leftMotor.on_for_rotations(-speed, ratio * dist, brake=True, block=False)
+    rightMotor.on_for_rotations(-speed, ratio * dist, brake=True, block=True)
+
+def section():
+    colour(1) = green[]
+    colour(2) = red[]
+    if (red[] != []) and (green[] == []):
+        if red[1] > 157.5:
+            farright()
+        else:
+            right()
+    elif (red[] == []) and (green[] != []):
+        if green[1] < 157.5:
+            farleft()
+        else:
+            left()
+    else: #no blocks detected
+        forwardTill(sped, 100, True)
+
+loop = asyncio.get_event_loop() #Run ultrasonic check alongside main code
+
+async def ultrasonic():
+    speed = -20
+    integral = 0
+    last_error = 0
+    kp = 0.5
+    ki = 0.01
+    kd = 0
+    while True:
+        if leftUltra.distance_centimeters > 50:
+            leftDist = 50
+        else:
+            leftDist = leftUltra.distance_centimeters
+        if rightUltra.distance_centimeters > 50:
+            rightDist = 50
+        else:
+            rightDist = rightUltra.distance_centimeters
+        if frontUltra.distance_centimeters > 50:
+            frontDist = 50
+        else:
+            frontDist = frontUltra.distance_centimeters
+
+        if frontDist < 10:
+            if leftDist < rightDist:
+                while frontDist < 40:
+                    leftMotor.on(50, brake=True, block=False)
+                    rightMotor.on(20, brake=True, block=False)
+            elif leftDist > rightDist:
+                while frontDist < 40:
+                    leftMotor.on(20, brake=True, block=False)
+                    rightMotor.on(50, brake=True, block=False)
+            else:
+                pass
+        error = leftDist - rightDist
+        integral = error + integral
+        derivative = error - last_error
+        value = error * kp + integral * ki + derivative * kd
+        if speed - value > 90:
+            leftSpeed = 90
+        elif speed - value < -90:
+            leftSpeed = -90
+        else:
+            leftSpeed = speed - value
+
+        if speed + value > 90:
+            rightSpeed = 90
+        elif speed + value < -90:
+            rightSpeed = -90
+        else:
+            rightSpeed = speed + value
+        leftMotor.on(leftSpeed, brake=True, block=False)
+        rightMotor.on(rightSpeed, brake=True, block=False)
+
+        await asyncio.sleep(1)  # Run every 1 second
+
+loop.create_task(ultrasonic())
+
+for i in range(3):
+    
+    forwardTill(sped, 70)
+    spinTurn(sped, a)
+    section()
+    section()
+    section()
+
+
+
+
+loop.run_forever()
